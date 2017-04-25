117da3d0818baf4a06e72066ce2e8752e347e3d8
yumotoatsushi <yumoto0502@icloud.com>
2017-04-25 07:33:26 +0000

    add column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
-class Message < ApplicationRecord
+class Task < ApplicationRecord
+    validates :content, presence: true, length: { maximum: 10 }
+     validates :status presence: true, length: { maximum: 255 }
+ def message_params
+    params.require(:task).permit(:content, :status)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170424061410) do
+ActiveRecord::Schema.define(version: 20170425072132) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
 end
