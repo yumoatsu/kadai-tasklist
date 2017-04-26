0e5f2ce64aaa108dadc5d7ad44b915bf6e1e322e
yumotoatsushi <yumoto0502@icloud.com>
2017-04-26 04:47:39 +0000

    add column

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
-    validates :content, presence: true, length: { maximum: 10 }
-     validates :status presence: true, length: { maximum: 255 }
- def message_params
-    params.require(:task).permit(:content, :status)
+  validates :content, presence: true, length: { maximum: 10 }
+  validates :status, presence: true, length: { maximum: 10 }
+def task_params
+params.require(:task).permit(:content, :status)
+end
 end
